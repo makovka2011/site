5ab340ad4e5eadc0d815c9aa0fadfcefb88e5f19
makovka2011 <47387956+makovka2011@users.noreply.github.com>
2022-06-22 20:34:46 +0500

    Add files via upload

---
+++ assets/js/javascript.js
@@ -0,0 +1,68 @@
+//alert('Привет, мир!')
+//console.log('Hello, world');
+//console.log(7+3);
+//let phone='+7064864367';
+//console.log(phone);
+//const COLOR_RED="#F0F0F0";
+//console.log(COLOR_RED)
+//const colors=['#F0F0F0', '#fffffff','#00000', 'red']
+//console.log(colors)
+//let int = 5;
+//let float= 5.98;
+//let int =010;
+//let=055;
+//int = 0xFF;
+//int = 0xB8;
+//let strl ='"ECMAScript"'
+/*function myfunc(str){
+    console.log(str);
+}
+const result = myfunc('JavaScript')
+console.log(result)
+
+function sum(a,b){
+    console.log(a+b)
+}
+sum(2,3);
+
+//document.getElementById("main").textContent = "Привет, мир! "
+const $btn = document.querySelector('#my-btn')
+$btn.onclick =function(){
+    alert('Вы нажали на кнопку');
+}*/
+/*function changeBgColor() {
+    document.body.style.backgroundColor = `rgb(${Math.round(Math.random()*255)}, ${Math.round(Math.random()*255)}, ${Math.round(Math.random()*255)})`;
+}
+document.onclick = changeBgColor;
+
+let text = 'Вкусный суп';
+let a = text.charAt(8);
+
+console.log(a);
+let a = ['Тише','мыши','кот на крыше'];
+let b = a.join(',');
+console.log(b);
+
+let i=1;
+while(i<=3){
+    console.log(i);
+    i++;
+}
+for(let i=1;i<=3;i++){
+    console.log(i);
+}
+ function CaleSum(a=0,b=0,c=5){
+    var d = a + b + c;
+    if (d<10){
+        return 0;
+    }else if(10<d && d<20){
+        return d + 50;
+    }else if (100< d){
+        return 100;
+    }
+    return d;
+ }
+var x=0, y=0 ,z=5
+let a= CaleSum(x,y,z);
+console.log(a);
+*/
